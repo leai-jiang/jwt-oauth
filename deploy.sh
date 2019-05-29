74b6068718a3323dbf5a3c48326feacb373bee6d
lei <jerry.ai@ucloud.cn>
2019-05-29 14:01:14 +0800

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@ export GOPATH=/data/golang
 export NODE_ENV=production
 
 docker build -t sparta .
+
+if docker ps -a | grep -i sparta; then
+    docker rm -f sparta
+fi
+
 docker run -p 5005:5005 -d sparta
