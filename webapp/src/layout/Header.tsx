520f52b8735be4e7c6fe1af36a32541046bf8308
lei <jerry.ai@ucloud.cn>
2019-05-29 15:26:15 +0800

    [stash]

--- webapp/src/layout/Header.tsx
+++ webapp/src/layout/Header.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
-import { Icon, Button } from "antd";
+import { Icon } from "antd";
 
 export const Header = (): JSX.Element => {
+	function loginGithub() {
+		window.location.href = "/api/login/github"
+	}
+
 	return (
 		<div className="header">
 			<div className="logo">Sparta</div>
-			<Icon type="user"/>
+			<div className="oauth-login">
+				<Icon type="github" onClick={loginGithub} />
+				<Icon type="wechat"/>
+			</div>
 		</div>
 	)
 };
